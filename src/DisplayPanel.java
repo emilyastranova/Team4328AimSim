c61f1ad33ae3f4b6be9fcdaf4accc96a134c9042
tyleraharrison <tyleraharrison@gmail.com>
2018-09-02 17:24:21 -0500

    Working prototype
    Need to add target and line

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -1,41 +1,47 @@
-import java.awt.Cursor;
 import java.awt.Graphics;
-import java.awt.Point;
-import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class DisplayPanel extends JPanel {
+public class DisplayPanel extends JPanel implements MouseMotionListener, ActionListener {
 
-	//make invisible cursor
-	BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-	Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
-	int x = 0;
-	int y = 0;
+	Robot robot = new Robot();
+	Timer loop = new Timer(10, this);
 	
+	MouseEvent mouse;
+
+	int mouseX = 100;
+	int mouseY = 100;
+
 	private BufferedImage image;
 
 	public DisplayPanel() {
 		setLayout(null);
-		setBounds(0, 0, 1264, 597);
-		//use invisible cursor
-		setCursor(blankCursor);
+		setBounds(0, 0, 1264, 578);
 		loadImages();
-		
+		addMouseMotionListener(this);
+		loop.start();
+
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		//draw Robot
-		g.drawImage(image, x - 50, y - 60, 100, 120, this);
+		// draw Robot
+		int x = (int) robot.getCoordinates().getX();
+		int y = (int) robot.getCoordinates().getY();
+		g.drawImage(robot.image, x, y, 100, 120, this);
 	}
-	
+
 	public void loadImages() {
 		try {
 			image = ImageIO.read(new File("resources/ShootingRobot.png"));
@@ -43,11 +49,28 @@ public class DisplayPanel extends JPanel {
 			System.err.println(ex.getMessage());
 		}
 	}
-	
-	public void mouseMoved(MouseEvent e) {
-		x = e.getX();
-		y = e.getY();
-		repaint();
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == loop) {
+			repaint();
+			robot.setCoordinates(mouseX-50, mouseY-50);
+		}
 	}
-	
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		mouseX = e.getX();
+		mouseY = e.getY();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -1,28 +1,32 @@
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class ImagePanel extends JPanel{
+public class ImagePanel extends JPanel {
 
-    private BufferedImage image;
+	BufferedImage image;
 
-    public ImagePanel() {
-       try {                
-          image = ImageIO.read(new File("resources/HorizontalLogoTrans.png"));
-       } catch (IOException ex) {
-           System.err.println(ex.getMessage());
-       }
-    }
+	public ImagePanel() {
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image, 70, 0, 300, 78, this); // see javadoc for more info on the parameters            
-    }
+		try {
+			image = ImageIO.read(new File("resources/HorizontalLogoTrans.png"));
+		} catch (IOException ex) {
+			System.err.println(ex.getMessage());
+		}
+
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(image, 70, 0, 300, 78, this); // see javadoc for more info on the parameters
+	}
 
 }
