41bae1d80ee5fdb48907f4cd10a2ffd54c5b85dd
Carson <Carson@Carson_Laptop>
2018-09-02 16:21:13 -0500

    Initial Carson Branch - started to use mouse coordinates

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -1,4 +1,8 @@
+import java.awt.Cursor;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,15 +12,19 @@ import javax.swing.JPanel;
 
 public class DisplayPanel extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
-
+	//make invisible cursor
+	BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+	Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+	int x = 0;
+	int y = 0;
+	
 	private BufferedImage image;
 
 	public DisplayPanel() {
 		setLayout(null);
 		setBounds(0, 0, 1264, 597);
+		//use invisible cursor
+		setCursor(blankCursor);
 		loadImages();
 		
 	}
@@ -25,7 +33,7 @@ public class DisplayPanel extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		//draw Robot
-		g.drawImage(image, 0, 0, 100, 120, this); // see javadoc for more info on the parameters
+		g.drawImage(image, x - 50, y - 60, 100, 120, this);
 	}
 	
 	public void loadImages() {
@@ -34,6 +42,12 @@ public class DisplayPanel extends JPanel {
 		} catch (IOException ex) {
 			System.err.println(ex.getMessage());
 		}
+	}
+	
+	public void mouseMoved(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+		repaint();
 	}
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-public class Main {
-
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
+public class Main extends JFrame {
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Main window = new Main();
-					window.frame.setVisible(true);
+					window.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,13 +33,12 @@ public class Main {
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Initialize the contents of the 
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setTitle("Team 4328 Aim Sim");
-		frame.setBounds(100, 100, 1280, 720);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Team 4328 Aim Sim");
+		setBounds(100, 100, 1280, 720);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e) {
@@ -58,17 +55,17 @@ public class Main {
 			e.printStackTrace();
 		}
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
-		int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
-		frame.setLocation(x, y);
-		frame.getContentPane().setLayout(null);
+		int x = (int) ((dimension.getWidth() - getWidth()) / 2);
+		int y = (int) ((dimension.getHeight() - getHeight()) / 2);
+		setLocation(x, y);
+		getContentPane().setLayout(null);
 		
 		JPanel buttonPanel = new ButtonPanel();
 		buttonPanel.setLocation(0, 580);
-		frame.getContentPane().add(buttonPanel);
+		getContentPane().add(buttonPanel);
 		
 		JPanel displayPanel = new DisplayPanel();
-		frame.getContentPane().add(displayPanel);
+		getContentPane().add(displayPanel);
 		
 	}
 }
